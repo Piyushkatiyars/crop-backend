6efb3013285f0645cbbdbe1de7ede83cb1e08614
Piyush katiyar <piyushkatiyar40@gmail.com>
2025-11-23 20:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
 import psycopg2
 import os
 
+# Load Model
+MODEL_PATH = "crop_disease_model.h5"
+model = tf.keras.models.load_model(MODEL_PATH)
+
 app = FastAPI()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
-
+# Home route
 @app.get("/")
 def home():
-    return {"message": "Crop Backend Running with PostgreSQL!"}
+    return {"message": "Crop Backend Running with ML Model + PostgreSQL!"}
 
+# Test DB
 @app.get("/test-db")
 def test_db():
     try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT now();")
-        result = cursor.fetchone()
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
         conn.close()
-        return {"db_time": result}
+        return {"status": "Database Connected Successfully"}
     except Exception as e:
-        return {"error": str(e)}
+        return {"status": "DB Error", "error": str(e)}
+
+# ======================
+#   PREDICT API
+# ======================
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+
+    image_bytes = await file.read()
+    img = Image.open(io.BytesIO(image_bytes)).resize((128, 128))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    class_id = np.argmax(prediction[0])
+
+    return {"prediction": int(class_id)}
+
+
